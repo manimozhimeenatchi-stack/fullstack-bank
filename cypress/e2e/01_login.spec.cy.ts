623eb58816b7d1a7848a3bf1840f33a393e73b3d
manimozhi <manimozhi.meenatchi@gmail.com>
2025-11-18 19:37:36 +0530

    Update 01_login.spec.cy.ts

--- cypress/e2e/01_login.spec.cy.ts
+++ cypress/e2e/01_login.spec.cy.ts
@@ -1,44 +1,40 @@
 import '@testing-library/cypress/add-commands';
 
-describe('Tela de login', () => {
+describe('Login Page', () => {
   beforeEach(() => {
     cy.clearLocalStorage();
     cy.visit(Cypress.config('baseUrl'));
   });
 
-  it('Verifica se todos os elementos estão visíveis na tela', () => {
+  it('Checks if all elements are visible on the screen', () => {
     cy.findByRole('img', { name: /ng\.cash/i }).should('exist');
     cy.findByText('Login').should('exist');
-    cy.findByPlaceholderText('Nome de usuário').should('exist');
-    cy.findByPlaceholderText('Senha').should('exist');
-    cy.findByRole('button', { name: /enviar/i }).should('exist');
-    cy.findByRole('button', { name: /criar conta/i }).should('exist');
+    cy.findByPlaceholderText('Username').should('exist');
+    cy.findByPlaceholderText('Password').should('exist');
+    cy.findByRole('button', { name: /submit/i }).should('exist');
+    cy.findByRole('button', { name: /create account/i }).should('exist');
     cy.findByText(
-      /full stack project desenvolvido por com next\.js e node\.js/i
+      /full stack project developed with next\.js and node\.js/i
     ).should('exist');
-    cy.findByText(/o nome de usuário ou a senha estão incorretos/i).should(
-      'not.exist'
-    );
+    cy.findByText(/the username or password is incorrect/i).should('not.exist');
   });
 
-  it('Verifica se a mensagem de erro aparece quando tentamos fazer login', () => {
-    cy.findByPlaceholderText('Nome de usuário').type('beyonceknowles');
-    cy.findByPlaceholderText('Senha').type('Qualquer_senha_123');
-    cy.findByRole('button', { name: /enviar/i }).click();
-    cy.findByText(/o nome de usuário ou a senha estão incorretos/i).should(
-      'exist'
-    );
+  it('Checks if error message appears when attempting to log in', () => {
+    cy.findByPlaceholderText('Username').type('beyonceknowles');
+    cy.findByPlaceholderText('Password').type('Any_password_123');
+    cy.findByRole('button', { name: /submit/i }).click();
+    cy.findByText(/the username or password is incorrect/i).should('exist');
   });
 
-  it('Verifica se redireciona para a dashboard após fazer login com credenciais válidas', () => {
-    cy.findByPlaceholderText('Nome de usuário').type('raphaelmartins');
-    cy.findByPlaceholderText('Senha').type('Bank_secret_123');
-    cy.findByRole('button', { name: /enviar/i }).click();
+  it('Checks if user is redirected to dashboard after logging in with valid credentials', () => {
+    cy.findByPlaceholderText('Username').type('raphaelmartins');
+    cy.findByPlaceholderText('Password').type('Bank_secret_123');
+    cy.findByRole('button', { name: /submit/i }).click();
     cy.url().should('be.equal', `${Cypress.config('baseUrl')}/dashboard`);
   });
 
-  it('Verifica se o botão de criar conta redireciona para a tela de cadastro', () => {
-    cy.findByRole('button', { name: /criar conta/i }).click();
+  it('Checks if create account button redirects to the registration page', () => {
+    cy.findByRole('button', { name: /create account/i }).click();
     cy.url().should('be.equal', `${Cypress.config('baseUrl')}/register`);
   });
 });
