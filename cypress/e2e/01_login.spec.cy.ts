5c98b4dd05ed7f578a8cef1b0798f4b3c0c9cd33
manimozhi <manimozhi.meenatchi@gmail.com>
2025-11-18 19:49:16 +0530

    Update 01_login.spec.cy.ts

--- cypress/e2e/01_login.spec.cy.ts
+++ cypress/e2e/01_login.spec.cy.ts
@@ -16,25 +16,29 @@ describe('Login Page', () => {
     cy.findByText(
       /full stack project developed with next\.js and node\.js/i
     ).should('exist');
+
     cy.findByText(/the username or password is incorrect/i).should('not.exist');
   });
 
-  it('Checks if error message appears when attempting to log in', () => {
+  it('Shows error message when login fails', () => {
     cy.findByPlaceholderText('Username').type('beyonceknowles');
     cy.findByPlaceholderText('Password').type('Any_password_123');
     cy.findByRole('button', { name: /submit/i }).click();
+
     cy.findByText(/the username or password is incorrect/i).should('exist');
   });
 
-  it('Checks if user is redirected to dashboard after logging in with valid credentials', () => {
-    cy.findByPlaceholderText('Username').type('manimozhi');
+  it('Redirects to dashboard after successful login', () => {
+    cy.findByPlaceholderText('Username').type('raphaelmartins');
     cy.findByPlaceholderText('Password').type('Bank_secret_123');
     cy.findByRole('button', { name: /submit/i }).click();
+
     cy.url().should('be.equal', `${Cypress.config('baseUrl')}/dashboard`);
   });
 
-  it('Checks if create account button redirects to the registration page', () => {
+  it('Redirects to registration page when clicking Create Account', () => {
     cy.findByRole('button', { name: /create account/i }).click();
+
     cy.url().should('be.equal', `${Cypress.config('baseUrl')}/register`);
   });
 });
